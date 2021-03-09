1d8c1c8bd235e3fddeb82b0bf0b7ff30eba11d95
tmoropou <tmoropou@ucsc.edu>
2021-03-09 14:16:27 -0800

    Small changes

---
+++ 151Final/Assets/SoundTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityOSC;
+
+public class SoundTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OSCHandler.Instance.Init();
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/trigger", "ready");
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/oscplayseq", 1);
+
+        StartCoroutine(Example());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(2);
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/oscplayseq", 1);
+    }
+}
