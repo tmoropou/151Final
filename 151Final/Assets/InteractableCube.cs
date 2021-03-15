59cd4b5507b7ece22a23cecef3f210c45b6d9ef9
tmoropou <tmoropou@ucsc.edu>
2021-03-15 16:01:36 -0700

    Added some UI and 3rd Channel

--- 151Final/Assets/InteractableCube.cs
+++ 151Final/Assets/InteractableCube.cs
@@ -8,6 +8,12 @@ public class InteractableCube : MonoBehaviour, IInteractable
 {
     int channelNumber = 1;
     bool channel3Playing = false;
+    TextMesh textObject;
+
+    void Start()
+    {
+        textObject = GameObject.Find("RadioText").GetComponent<TextMesh>();
+    }
 
     public void Interact()
     {
@@ -21,6 +27,7 @@ public class InteractableCube : MonoBehaviour, IInteractable
         switch (channelNumber)
         {
             case 1:
+                textObject.text = "Radio On: Channel 1";
                 OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel1", 1);
                 if (channel3Playing)
                 {
@@ -30,13 +37,20 @@ public class InteractableCube : MonoBehaviour, IInteractable
                 channelNumber = 2;
                 break;
             case 2:
+                textObject.text = "Radio On: Channel 2";
                 OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel2", 1);
                 OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel1", 0);
                 channelNumber = 3;
                 break;
             case 3:
+                textObject.text = "Radio On: Channel 3";
                 OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel3", 1);
                 OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel2", 0);
+                channelNumber = 4;
+                break;
+            case 4:
+                textObject.text = "Radio Off";
+                OSCHandler.Instance.SendMessageToClient("pd", "/unity/playchannel3", 0);
                 channelNumber = 1;
                 channel3Playing = true;
                 break;
